5a464257176f9245d48f8d57a163fd2df5e84679
vrac10 <vrachit106@gmail.com>
2023-11-19 01:46:01 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 const port = 8000;
 
 const corsOptions = {
-    origin:'http://localhost:3000', 
+    origin:'https://musiq-0xmd.onrender.com', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
