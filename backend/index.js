9af0557eeaba3ff20b68414668f8a9c53518e9de
vrac10 <vrachit106@gmail.com>
2023-11-19 02:05:40 +0530

    Added option

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,9 @@ const app = express();
 const port = 8000;
 
 const corsOptions = {
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+    origin: '*', 
+    credentials: true,            // access-control-allow-credentials: true
+    optionSuccessStatus: 200
 }
 
 app.use(cors(corsOptions));
